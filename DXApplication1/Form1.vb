051fa1291ab7abfc105af90aa810445e2fe1ce78
Erhard Wilder <ewilder@hotmail.de>
2021-11-29 15:49:32 +0100

    Projektdateien hinzufügen.

---
+++ DXApplication1/Form1.vb
@@ -0,0 +1,36 @@
+﻿Imports System.ComponentModel
+Imports System.ComponentModel.DataAnnotations
+Imports DevExpress.XtraBars
+Imports DevExpress.XtraEditors
+
+Partial Public Class Form1
+    ' Bemerkung Form1[Entwurf] darf nicht benutzt werden, 2 unterschiedliche Vorgänge: Systemabsturz möglich
+    Public Sub New()
+        InitializeComponent()
+
+        Dim dataSource As BindingList(Of Customer) = GetDataSource()
+        gridControl.DataSource = dataSource
+        bsiRecordsCount.Caption = "RECORDS : " & dataSource.Count
+    End Sub
+    Private Sub bbiPrintPreview_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs) Handles bbiPrintPreview.ItemClick
+        gridControl.ShowRibbonPrintPreview()
+    End Sub
+    Public Function GetDataSource() As BindingList(Of Customer)
+        Dim result As New BindingList(Of Customer)()
+        result.Add(New Customer() With {.ID = 1, .Name = "ACME", .Address = "2525 E El Segundo Blvd", .City = "El Segundo", .State = "CA", .ZipCode = "90245", .Phone = "(310) 536-0611"})
+        result.Add(New Customer() With {.ID = 2, .Name = "Electronics Depot", .Address = "2455 Paces Ferry Road NW", .City = "Atlanta", .State = "GA", .ZipCode = "30339", .Phone = "(800) 595-3232"})
+        Return result
+    End Function
+    Public Class Customer
+        <Key, Display(AutoGenerateField:=False)>
+        Public Property ID() As Integer
+        <Required>
+        Public Property Name() As String
+        Public Property Address() As String
+        Public Property City() As String
+        Public Property State() As String
+        <Display(Name:="Zip Code")>
+        Public Property ZipCode() As String
+        Public Property Phone() As String
+    End Class
+End Class
